ea648ee45114b6538ba55266af3d3b6ed70026b8
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2025-04-20 00:35:35 +0300

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-import psutil
-import socket
-
-# List all network interfaces and their associated IPs
-for iface, addrs in psutil.net_if_addrs().items():
-    print(f"Interface: {iface}")
-    for addr in addrs:
-        if addr.family == socket.AF_INET:
-            print(f"  IP address: {addr.address}")

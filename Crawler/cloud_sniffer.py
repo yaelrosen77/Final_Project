7571c3e4fca12555a76e7b190966f46e75ba0579
yaelrosen77 <yaelrosen10@gmail.com>
2025-08-18 16:14:24 +0300

    addlink

--- Crawler/cloud_sniffer.py
+++ Crawler/cloud_sniffer.py
@@ -77,7 +77,7 @@ def sniff_cloud_upload(url, play_class="", pre_class=""):
     print(f"✅ Upload capture done: {pcap_file}")
 
 def sniff_all_cloud():
-    links = load_links_from_excel("Cloud")[13:]
+    links = load_links_from_excel("Cloud")[18:]
     for url, play_class, pre_class in links:
         sniff_cloud_upload(url, play_class, pre_class)
 
